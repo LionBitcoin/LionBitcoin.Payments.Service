daa56f7d2911d9afd8bc7e3e9266def5082a6279
SandroKavlashvili <kavlashvilisandro1@gmail.com>
2025-03-10 23:18:53 +0400

    lfjbnasijklfghbakedjh

--- LionBitcoin.Payments.Service.Application/Features/Customers/Commands/RegisterCustomer/RegisterCustomerCommandHandler.cs
+++ LionBitcoin.Payments.Service.Application/Features/Customers/Commands/RegisterCustomer/RegisterCustomerCommandHandler.cs
@@ -1,13 +1,29 @@
+using System.Data;
 using System.Threading;
 using System.Threading.Tasks;
+using LionBitcoin.Payments.Service.Application.Repositories;
+using LionBitcoin.Payments.Service.Application.Repositories.Base;
 using MediatR;
 
 namespace LionBitcoin.Payments.Service.Application.Features.Customers.Commands.RegisterCustomer;
 
 public class RegisterCustomerCommandHandler : IRequestHandler<RegisterCustomerCommand, RegisterCustomerResponse>
 {
-    public Task<RegisterCustomerResponse> Handle(RegisterCustomerCommand request, CancellationToken cancellationToken)
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly ICustomerRepository _customerRepository;
+
+    public RegisterCustomerCommandHandler(IUnitOfWork unitOfWork, ICustomerRepository customerRepository)
     {
-        throw new System.NotImplementedException();
+        _unitOfWork = unitOfWork;
+        _customerRepository = customerRepository;
+    }
+
+    public async Task<RegisterCustomerResponse> Handle(RegisterCustomerCommand request, CancellationToken cancellationToken)
+    {
+        using IDbTransaction transaction = await _unitOfWork.BeginTransactionAsync(cancellationToken);
+
+        //TODO: ProduceCustomerCreatedEvent
+
+        return new RegisterCustomerResponse();
     }
 }
