a13ab85c6c44ffe070f9d2579c14db71eaa7a65c
SandroKavlashvili <kavlashvilisandro1@gmail.com>
2025-03-14 00:56:49 +0400

    configure eventing library

--- LionBitcoin.Payments.Service.Infrastructure/InfrastructureExtensions.cs
+++ LionBitcoin.Payments.Service.Infrastructure/InfrastructureExtensions.cs
@@ -1,3 +1,5 @@
+using LionBitcoin.Payments.Service.Application.Services.Abstractions;
+using LionBitcoin.Payments.Service.Infrastructure.Services;
 using LionBitcoin.Payments.Service.Infrastructure.Services.Configs;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,34 +11,73 @@ public static class InfrastructureExtensions
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        
+        services.AddCap(configuration);
+        services.AddScoped<IEventsService, EventsService>();
+
         return services;
     }
 
     private static IServiceCollection AddCap(this IServiceCollection services, IConfiguration configuration)
     {
-        services.Configure<EventsRabbitMqConfig>(configuration.GetSection("EventsRabbitMqSettings"));
-        string connectionString = GetConnectionString(configuration);
+        EventsRabbitMqConfig eventsRabbitMqConfig = ConfigureAndGetEventsRabbitMqConfig(services, configuration);
+
+        string connectionString = RegisterAndGetConnectionString(services, configuration);
         services.AddCap(options =>
         {
             options.UsePostgreSql(connectionString);
             options.UseRabbitMQ(rabbitOptions =>
             {
-                rabbitOptions.HostName = "";
-                rabbitOptions.VirtualHost = "";
-                rabbitOptions.Port = 12;
-                rabbitOptions.UserName = "";
-                rabbitOptions.Password = "";
+                rabbitOptions.HostName = eventsRabbitMqConfig.Host;
+                rabbitOptions.VirtualHost = eventsRabbitMqConfig.VirtualHost;
+                rabbitOptions.Port = eventsRabbitMqConfig.Port;
+                rabbitOptions.UserName = eventsRabbitMqConfig.UserName;
+                rabbitOptions.Password = eventsRabbitMqConfig.Password;
             });
         });
 
         return services;
     }
 
-    private static string GetConnectionString(IConfiguration configs)
+    private static EventsRabbitMqConfig ConfigureAndGetEventsRabbitMqConfig(IServiceCollection services, IConfiguration configuration)
     {
-        NpgsqlConnectionStringBuilder connectionStringBuilder = new NpgsqlConnectionStringBuilder();
-        configs.GetSection("PaymentsServiceDb").Bind(connectionStringBuilder);
+        IConfigurationSection section = configuration.GetSection("EventsRabbitMqSettings");
+        services.AddOptions<EventsRabbitMqConfig>()
+            .Bind(section)
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+        EventsRabbitMqConfig config = new EventsRabbitMqConfig();
+        section.Bind(config);
+        return config;
+    }
+
+    private static string RegisterAndGetConnectionString(IServiceCollection services, IConfiguration configs)
+    {
+        EventsDbConfig config = RegisterAndGetEventsDatabaseConfig(services, configs);
+
+        NpgsqlConnectionStringBuilder connectionStringBuilder = new NpgsqlConnectionStringBuilder()
+        {
+            Host = config.Host,
+            Pooling = config.Pooling,
+            MaxPoolSize = config.MaxPoolSize,
+            Username = config.Username,
+            Password = config.Password,
+            Port = config.Port,
+            Database = config.Database,
+        };
         return connectionStringBuilder.ConnectionString;
+    }
+
+    private static EventsDbConfig RegisterAndGetEventsDatabaseConfig(IServiceCollection services, IConfiguration configs)
+    {
+        IConfigurationSection section = configs.GetSection("PaymentsServiceDb");
+        services.AddOptions<EventsDbConfig>()
+            .Bind(section)
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+        EventsDbConfig config = new EventsDbConfig();
+        section.Bind(config);
+        return config;
     }
 }
--- LionBitcoin.Payments.Service.Infrastructure/Services/Configs/EventsDbConfig.cs
+++ LionBitcoin.Payments.Service.Infrastructure/Services/Configs/EventsDbConfig.cs
@@ -2,20 +2,26 @@ using System.ComponentModel.DataAnnotations;
 
 namespace LionBitcoin.Payments.Service.Infrastructure.Services.Configs;
 
-public class EventsRabbitMqConfig
+public class EventsDbConfig
 {
     [Required]
     public string Host { get; set; }
 
     [Required]
-    public string VirtualHost { get; set; }
+    public bool Pooling { get; set; }
+
+    [Required]
+    public int MaxPoolSize { get; set; }
+
+    [Required]
+    public string Username { get; set; }
 
     [Required]
     public string Password { get; set; }
 
     [Required]
-    public string UserName { get; set; }
+    public int Port { get; set; }
 
     [Required]
-    public int Port { get; set; }
+    public string Database { get; set; }
 }
