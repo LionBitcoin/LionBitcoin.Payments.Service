044594484561309701d1e94bf39e8982b339daab
SandroKavlashvili <kavlashvilisandro1@gmail.com>
2025-03-16 12:12:29 +0400

    BlockThreshold for blocks explorer

--- LionBitcoin.Payments.Service.Application/Repositories/Base/IUnitOfWork.cs
+++ LionBitcoin.Payments.Service.Application/Repositories/Base/IUnitOfWork.cs
@@ -1,7 +1,5 @@
-using System.Data;
 using System.Threading;
 using System.Threading.Tasks;
-using LionBitcoin.Payments.Service.Application.Domain.Events.Base;
 
 namespace LionBitcoin.Payments.Service.Application.Repositories.Base;
 
--- LionBitcoin.Payments.Service.Application/Shared/PaymentServiceSettings.cs
+++ LionBitcoin.Payments.Service.Application/Shared/PaymentServiceSettings.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LionBitcoin.Payments.Service.Application.Shared;
 
 public class PaymentServiceSettings
 {
+    [Required]
     public Network Network { get; set; }
 
+    [Required]
     public string AccountExtendedPublicKey { get; set; }
+
+    [Required]
+    public long ThresholdBlockIndex { get; set; }
 }
